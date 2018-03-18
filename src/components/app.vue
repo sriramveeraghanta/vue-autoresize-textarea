a3ac66de6ed46b90673666336e9dc1000afe19ac
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2018-03-19 01:47:58 +0530

    revert back to plan enter mode

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <textarea v-model="localValue" v-on:keyup="resize" v-on:keydown="inputHandler" ></textarea>
+  <textarea v-model="localValue" v-on:keyup="resize"></textarea>
 </template>
 
 <script>
@@ -20,14 +20,6 @@ export default {
     resize: function(event) {
       autosize(event.target)
     },
-    inputHandler: function(event) {
-      if (event.keyCode === 13 && !event.shiftKey) {
-        //console.log("enter key pressed")
-        event.preventDefault();
-        // sending Event data back to parent node
-        this.$emit('onEnterKeydown', event)
-      }
-    }
   },
   watch: {
     value (newVal, oldVal) {
