c7db0d1a6e3b8beab21932e4fc956908eb0a042a
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2018-03-18 19:37:14 +0530

    enter key dual functionality

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,5 +1,5 @@
 <template>
-    <textarea v-model="localValue" v-on:keyup="resize" ></textarea>
+  <textarea v-model="localValue" v-on:keyup="resize" v-on:keydown="inputHandler" ></textarea>
 </template>
 
 <script>
@@ -17,9 +17,17 @@ export default {
     autosize(this.$el)
   },
   methods: {
-  	resize: function(event) {
-  		autosize(event.target)
-  	}
+    resize: function(event) {
+      autosize(event.target)
+    },
+    inputHandler: function(event) {
+      if (event.keyCode === 13 && !event.shiftKey) {
+        //console.log("enter key pressed")
+        event.preventDefault();
+        // sending Event data back to parent node
+        this.$emit('onEnterKeydown', event)
+      }
+    }
   },
   watch: {
     value (newVal, oldVal) {
